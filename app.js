e785d996f48bd88e584fb3177f17691a0b7dc10f
ramy <ramy.niranjan@gmail.com>
2019-12-24 01:36:45 +0100

    add more files

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 
 
-const picArr = ['./wela1.jpg', './wela2.jpg', './wela3.jpg', './wela4.jpg', './wela5.jpg',]
+const picArr = ['./wela1.jpg', './wela2.jpg', './wela3.jpg', './wela4.jpg', './wela5.jpg','./wela6.jpg','./wela7.jpg','./wela8.jpg','./wela9.jpg','./wela10.jpg']
 
 function snowFlak(){
-    let wela=document.createElement('div');
-    let snowFlak=document.createElement('i');
-    wela.classList.add('wela')
-    snowFlak.classList.add('fas')
-    snowFlak.classList.add('fa-snowflake')
-    wela.style.backgroundImage = `url('${picArr[Math.floor((Math.random() * picArr.length))]}')` 
   
-    wela.style.left=Math.random()* window.innerWidth+'px'
+    let snowFlak=document.createElement('i');
+    snowFlak.classList.add('fas')
+    snowFlak.classList.add('fa-snowflake') 
     snowFlak.style.left=Math.random()* window.innerWidth+'px'
-    wela.style.animationDuration=Math.random()* 3 +6 +'s'
     snowFlak.style.animationDuration=Math.random()* 3 +6 +'s'
     snowFlak.style.opacity=Math.random()
-   
-  
-    document.body.appendChild(wela)
     document.body.appendChild(snowFlak)
 
     setTimeout(() => {
-         wela.remove()
+        snowFlak.remove()
     }, 9000);
 }
+function wela(){
+    let wela = document.createElement('div');
+    wela.classList.add('wela')
+    wela.style.backgroundImage = `url('${picArr[Math.floor((Math.random() * picArr.length))]}')`
 
+    wela.style.left = Math.random() * window.innerWidth + 'px'
+    wela.style.animationDuration = Math.random() * 3 + 6 + 's'
+    document.body.appendChild(wela)
+
+    setTimeout(() => {
+        wela.remove()
+    }, 9000);
+}
+setInterval(wela,500);
 setInterval(snowFlak,100);
